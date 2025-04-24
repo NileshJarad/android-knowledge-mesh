de47cf734ee273ef813b57e1acebf3e9bf146d80
Nilesh Jarad <nlshjarad7@gmail.com>
2025-04-24 11:17:37 +0530

    Update CoroutineScopeInBuildExample.kt
    Added output for the corroutine scope

--- android/src/main/kotlin/coroutine/CoroutineScopeInBuildExample.kt
+++ android/src/main/kotlin/coroutine/CoroutineScopeInBuildExample.kt
@@ -24,3 +24,13 @@ fun main() = runBlocking {
 
     println("Main coroutine ends")
 }
+
+
+/****
+Main coroutine starts
+Coroutine 1 executed
+Exception in thread "main" java.lang.Exception: Coroutine 2 failed
+ at FileKt$main$1$1$2.invokeSuspend (File.kt:15) 
+ at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith (ContinuationImpl.kt:33) 
+ at kotlinx.coroutines.DispatchedTaskKt.resume (DispatchedTask.kt:235) 
+***/
